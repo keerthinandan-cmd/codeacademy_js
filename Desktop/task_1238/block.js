8c21db9ade73b6c582f27956add504344b4db2be
Keerthi Nandan <keerthi.nandan@inxsasia.com>
2021-04-15 19:26:23 +0530

    next commit

---
+++ Desktop/task_1238/block.js
@@ -0,0 +1,5 @@
+const city='New York City';
+const logCitySkyline=()=>{
+  let skyscraper='Empire State Building';
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+}
---
+++ Desktop/task_1238/global_scope.js
@@ -0,0 +1,7 @@
+var satellite='The Moon';
+var galaxy='The Milky Way';
+var stars='North Star';
+const callMyNightSky=()=>{
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+console.log(callMyNightSky());
+}
